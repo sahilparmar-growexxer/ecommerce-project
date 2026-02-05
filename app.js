b481fc706b46a7e757e8b4b31a0a513b71628269
Sahil Parmar <sahil.parmar@growexx.com>
2026-02-05 16:47:56 +0530

    Add server startup configuration

--- app.js
+++ app.js
@@ -3,4 +3,10 @@ const app = express();
 app.get('/', (req, res) => {
 res.send('Welcome to the App');
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+console.log(`Server running on port ${PORT}`);
+});
+
 module.exports = app;
