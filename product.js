8a0a84d760fb1e836f5615656376c0d10466d2e0
Sahil Parmar <sahil.parmar@growexx.com>
2026-02-05 16:33:19 +0530

    whyforcenecessary

--- product.js
+++ product.js
@@ -5,9 +5,17 @@ this.price = price;
 this.discount = 0;
 }
 getPrice() {
+<<<<<<< HEAD
 return this.price - (this.price * this.discount / 100);
+<<<<<<< HEAD
 const tax = 0.18; // 18% tax
 return this.price + (this.price * tax);
+=======
+=======
+const tax = 0.18; // 18% tax
+return this.price + (this.price * tax);
+>>>>>>> ae57514 (Add tax calculation)
+>>>>>>> 24359cb (implement user login with validation)
 }
 }
 module.exports = Product;
